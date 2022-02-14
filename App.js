13e95451ab30f0687e215ccf5ea065232b0a8965
thegreytangent <thegreytangent@gmail.com>
2022-02-14 08:00:48 +0800

    Learning to manipulate images

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image 
+      fadeDuration={1000}
+        blurRadius= {3}
+        source={{
+        width: 200,
+        height: 300,
+      
+        uri: "https://picsum.photos/200/300"
+      }}/>
+      <Text>Hello react</Text>
       <StatusBar style="auto" />
     </View>
   );
